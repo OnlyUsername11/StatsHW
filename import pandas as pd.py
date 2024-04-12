559f806dfd2ec97efa87db925c567489d6d48082
OnlyUsername11 <150260253+OnlyUsername11@users.noreply.github.com>
2024-04-12 16:34:10 +0300

    Add files via upload

---
+++ import pandas as pd.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import statistics as stats
+
+df = pd.read_csv(r''C:\Users\Alimrzadmr\Desktop\Cleaned_ships_data.csv'')
+
+
